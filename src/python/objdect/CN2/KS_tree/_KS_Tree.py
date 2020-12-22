5ebe46bbaa22144ed34c883bed3df06e4651552d
jtzanetatos <58182829+jtzanetatos@users.noreply.github.com>
2020-12-23 00:31:05 +0200

    Finalize KS Tree function

--- src/python/objdect/CN2/KS_tree/_KS_Tree.py
+++ src/python/objdect/CN2/KS_tree/_KS_Tree.py
@@ -4,16 +4,17 @@
 
 import numpy as np
 from scipy.stats import entropy
+from _Null_Hypothesis import NullHypothesis
+from _KS_Significance_Test import KSTest
 
 # --------------------------------------------------------------------------- #
 # Implement adaptive windows of interest by means of KS statistical test
 # --------------------------------------------------------------------------- #
-def KSAdaptiveWindows(final_win_vals, final_win_loc, hist_norm,
-              win_vals, win_binloc, over_vals, over_binloc):
+def KSAdaptiveWindows(CN2winVals, CN2winLoc, histNorm, winVals, winLoc):
     '''
-    Function that utilizes the Kolmogorov-Smirnov statistical test in order to
-    implement adaptive windows that contain the resulting histograms of each
-    colour channel. In its current implementation, the windows of interest,
+    Adaptive windows utilizing the Kolmogorov-Smirnov statistical test 
+    containing the resulting histograms of each colour channel.
+    In its current implementation, the windows of interest,
     resulting from the Rule Based Tree (rule_tree) function are utilized.
     
     The Null Hypothesis (H0) is defined as the shape of plain & overlapping 
@@ -24,192 +25,181 @@ def KSAdaptiveWindows(final_win_vals, final_win_loc, hist_norm,
     windows do not overlap significantly, therefore no need to expand current 
     window, keep current window as is
     
-    In its current implementation, it is assumed that the input arrays have
-    dimmentions of (8x32x3); however the only restriction is the number of rows
-    that the algorithm takes into consideration.
+    It is assumed that the input arrays have dimentions of (8x32x3);
+    however the only restriction is the number of rows that the algorithm
+    takes into consideration.
     
     '''
     # If all colour channels present
-    if np.any(final_win_loc[:, 0]) != 0 and np.any(final_win_loc[:, 1]) != 0 \
-                              and np.any(final_win_loc[:, 2]) != 0:
+    if ( np.sum(CN2winLoc[:, 0]) > 0 and
+         np.sum(CN2winLoc[:, 1]) > 0 and
+         np.sum(CN2winLoc[:, 2]) > 0 ):
         # KS Significance test
-        signCrit = KSTest(sigWind)
+        signCrit = KSTest(CN2winVals)
         
         # If Alternative Hypothesis (H1) true
         if ( signCrit[0] >= 0.5 and
              signCrit[1] >= 0.5 and
              signCrit[2] >= 0.5):
-            
-            # TODO: Utilize alternative hypothesis function
-            
+            # Return input windows
+            outWinvals = CN2winVals
+            outBinlocs = CN2winLoc
             
         # If Null Hypothesis (H0) true
         else:
-            
-            # TODO: NULL Hypothesis call
+            # Expanded windows
+            outWinvals,outBinlocs = NullHypothesis(CN2winLoc, CN2winVals)
             
     # Determine if Blue channel is present
-    if np.sum(final_win_loc[:, 0]) > 0:
+    if np.sum(CN2winLoc[:, 0]) > 0:
         # Check if Red & Blue channels present only
-        if np.sum(final_win_loc[:, 2]) > 0:
-            
+        if np.sum(CN2winLoc[:, 2]) > 0:
             # KS Significance test
-            signCrit = KSTest(sigWind)
+            signCrit = KSTest(CN2winVals)
             
             # If Alternative Hypothesis (H1) true
             if signCrit.all() >= 0.5:
-                
-                # TODO: Utilize alternative hypothesis function
-                
+                # Return input windows
+                outWinvals = CN2winVals
+                outBinlocs = CN2winLoc
             # If Null Hypothesis (H0) true
             else:
-                
-                # TODO: NULL Hypothesis call
-                
-        
+                # Expanded windows
+                outWinvals,outBinlocs = NullHypothesis(CN2winLoc, CN2winVals)
         # Check if Blue & Green channels are present
-        elif np.sum(final_win_loc[:, 1]) > 0:
-            
+        elif np.sum(CN2winLoc[:, 1]) > 0:
             # KS Significance test
-            signCrit = KSTest(sigWind)
+            signCrit = KSTest(CN2winVals)
             
             # If Alternative Hypothesis (H1) true
             if ( signCrit[0] >= 0.5 and
                  signCrit[1] >= 0.5 ):
                 
-                # TODO: Utilize alternative hypothesis function
-                
+                # Return input windows
+                outWinvals = CN2winVals
+                outBinlocs = CN2winLoc
             # If Null Hypothesis (H0) true
             else:
-                
-                # TODO: NULL Hypothesis call
-                
-        
+                # Expanded windows
+                outWinvals,outBinlocs = NullHypothesis(CN2winLoc, CN2winVals)
         # If only Blue Channel present
         else:
-            
             # KS Significance test
-            signCrit = KSTest(sigWind)
+            signCrit = KSTest(CN2winVals)
             
             # If Alternative Hypothesis (H1) true
             if signCrit >= 0.5:
-                
-                # TODO: Utilize alternative hypothesis function
-                
+                # Return input windows
+                outWinvals = CN2winVals
+                outBinlocs = CN2winLoc
             # If Null Hypothesis (H0) true
             else:
-                
-                # TODO: NULL Hypothesis call
-                
+                # Expanded windows
+                outWinvals,outBinlocs = NullHypothesis(CN2winLoc, CN2winVals)
     # If Green Channel present
-    elif np.sum(final_win_loc[:, 1]) > 0:
+    elif np.sum(CN2winLoc[:, 1]) > 0:
         # If Green & Blue channels present
-        if np.sum(final_win_loc[:, 0]) > 0:
-            
+        if np.sum(CN2winLoc[:, 0]) > 0:
             # KS Significance test
-            signCrit = KSTest(sigWind)
+            signCrit = KSTest(CN2winVals)
             
             # If Alternative Hypothesis (H1) true
             if ( signCrit[0] >= 0.5 and
                  signCrit[1] >= 0.5 ):
                 
-                # TODO: Utilize alternative hypothesis function
-                
+                # Return input windows
+                outWinvals = CN2winVals
+                outBinlocs = CN2winLoc
             # If Null Hypothesis (H0) true
             else:
-               
-                # TODO: NULL Hypothesis call
-                
+                # Expanded windows
+                outWinvals,outBinlocs = NullHypothesis(CN2winLoc, CN2winVals)
         # If Green & Red channels present
-        elif np.sum(final_win_loc[:, 2]) > 0:
-            
+        elif np.sum(CN2winLoc[:, 2]) > 0:
             # KS Significance test
-            signCrit = KSTest(sigWind)
+            signCrit = KSTest(CN2winVals)
             
             # If Alternative Hypothesis (H1) true
             if ( signCrit[0] >= 0.5 and
                  signCrit[1] >= 0.5 ):
                 
-                # TODO: Utilize alternative hypothesis function
-                
+                # Return input windows
+                outWinvals = CN2winVals
+                outBinlocs = CN2winLoc
             # If Null Hypothesis (H0) true
             else:
-                
-                # TODO: NULL Hypothesis call
-                
+                # Expanded windows
+                outWinvals,outBinlocs = NullHypothesis(CN2winLoc, CN2winVals)
         # If Green Channel only
         else:
-            
             # KS Significance test
-            signCrit = KSTest(sigWind)
+            signCrit = KSTest(CN2winVals)
             
             # If Alternative Hypothesis (H1) true
             if signCrit >= 0.5:
-                
-                # TODO: Utilize alternative hypothesis function
-                
+                # Return input windows
+                outWinvals = CN2winVals
+                outBinlocs = CN2winLoc
             # If Null Hypothesis (H0) true
             else:
-                
-                # TODO: NULL Hypothesis call
-                
+                # Expanded windows
+                outWinvals,outBinlocs = NullHypothesis(CN2winLoc, CN2winVals)
     # If Red Chanel present
-    elif np.sum(final_win_loc[:, 2]) > 0:
+    elif np.sum(CN2winLoc[:, 2]) > 0:
         # If Red & Green Channels present
-        if np.sum(final_win_loc[:, 1]) > 0:
-            
+        if np.sum(CN2winLoc[:, 1]) > 0:
             # KS Significance test
-            signCrit = KSTest(sigWind)
+            signCrit = KSTest(CN2winVals)
             
             # If Alternative Hypothesis (H1) true
             if ( signCrit[0] >= 0.5 and
                  signCrit[1] >= 0.5 ):
                 
-                # TODO: Utilize alternative hypothesis function
+                # Return input windows
+                outWinvals = CN2winVals
+                outBinlocs = CN2winLoc
                 
             # If Null Hypothesis (H0) true
             else:
-                
-                # TODO: NULL Hypothesis call
-                
+                # Expanded windows
+                outWinvals,outBinlocs = NullHypothesis(CN2winLoc, CN2winVals)
         # If Red & Blue channels present
-        elif np.sum(final_win_loc[:, 0]) > 0:
-            
+        elif np.sum(CN2winLoc[:, 0]) > 0:
             # KS Significance test
-            signCrit = KSTest(sigWind)
+            signCrit = KSTest(CN2winVals)
             
             # If Alternative Hypothesis (H1) true
             if ( signCrit[0] >= 0.5 and
                  signCrit[1] >= 0.5 ):
                 
-                # TODO: Utilize alternative hypothesis function
+                # Return input windows
+                outWinvals = CN2winVals
+                outBinlocs = CN2winLoc
                 
             # If Null Hypothesis (H0) true
             else:
-                
-                # TODO: NULL Hypothesis call
-                
+                # Expanded windows
+                outWinvals,outBinlocs = NullHypothesis(CN2winLoc, CN2winVals)
         # If only Red channel present
         else:
-            
             # KS Significance test
-            signCrit = KSTest(sigWind)
+            signCrit = KSTest(CN2winVals)
             
             # If Alternative Hypothesis (H1) true
             if signCrit >= 0.5:
-                
-                # TODO: Utilize alternative hypothesis function
+                # Return input windows
+                outWinvals = CN2winVals
+                outBinlocs = CN2winLoc
                 
             # If Null Hypothesis (H0) true
             else:
-                
-                # TODO: NULL Hypothesis call
-                
+                # Expanded windows
+                outWinvals,outBinlocs = NullHypothesis(CN2winLoc, CN2winVals)
     # If no colour channel present return zeros
     else:
         # Initialize arrays
-        out_binlocs = np.zeros((32, 3), dtype=np.uint8)
-        out_win_vals = np.zeros((32, 3), dtype=np.float32)
+        outBinlocs = np.zeros((32, 3), dtype=np.uint8)
+        outWinvals = np.zeros((32, 3), dtype=np.float32)
     
     # Return resulting arrays
-    return out_binlocs, out_win_vals
+    return outBinlocs, outWinvals
