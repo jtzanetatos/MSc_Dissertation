c85c3ae2630a1146887ef7e5761166191ad53bc8
jtzanetatos <58182829+jtzanetatos@users.noreply.github.com>
2020-12-26 17:14:28 +0200

    Complete rework of KS Significance test

--- src/python/objdect/CN2/KS_tree/_KS_Significance_Test.py
+++ src/python/objdect/CN2/KS_tree/_KS_Significance_Test.py
@@ -7,39 +7,56 @@ Created on Sat Dec  5 19:54:29 2020
 
 import numpy as np
 
-def KSTest(sigWind):
+def KSTest(CN2winLoc, winVals, winLoc):
     '''
-    Kolmogorov-Smirnoff test on sliding windows to determine significance on 
-    distributions.
+    Kolmogorov Smirnov statistical significance test between window derived
+    from CN2 tree and its adjacent slidding windows.
     
     Parameters
     ----------
-    sigWind : float32 multidimensional array
-        Array containing sliding windows.
+    CN2winLoc : uint8 array
+        Resulting window locations from CN2 tree.
+    winVals : float32 array
+        Normalized slidding windows values of input frame's histogram.
+    winLoc : uint8 array
+        Slidding windows locations of input frame's histogram.
     
     Returns
     -------
     signCrit : float32
-        Results of KS test/significance.
+        KS significance test result.
     
     '''
-    
-    # Get input shape
-    try:
-        row, colm = sigWind.shape
-    
-    except ValueError:
-        # Set column to 1
-        colm = 1
-    
-    # Initialize array to store KS test results
-    signCrit = np.zeros(colm, dtype=np.float32)
-    
-    # Evaluate CDF of sliding windows
-    for i in range(colm):
-        plainCDF = np.cumsum(sigWind[:, ]) / 256
-        overCDF = np.cumsum(sigWind[:, ]) / 256
+    # First window comparison
+    if CN2winLoc[0] == 0:
+        plainCDF = np.cumsum(winVals[0, :])
+        overCDF = np.cumsum(winVals[1, :])
         
-        signCrit[i] = np.max(np.abs(plainCDF - overCDF))
+        # Significance test on two windows
+        signCrit = np.max(np.abs(plainCDF - overCDF))
+    # Last window comparison
+    elif CN2winLoc[0] == 224:
+        plainCDF = np.cumsum(winVals[-1, :])
+        overCDF = np.cumsum(winVals[winVals.shape[0] -2, :]) 
+        
+        # Significance test on two windows
+        signCrit = np.max(np.abs(plainCDF - overCDF))
+    # Remaining windows
+    else:
+        # Locate window
+        idx = np.where(CN2winLoc[0] == winLoc[:, 0])
+        
+        # Evaluate CDF of current window
+        currCDF = np.cumsum(winVals[idx, :])
+        
+        # Evaluate CDF of previous window
+        prevCDF = np.cumsum(winVals[idx-1, :])
+        
+        # Evaluate CDF of next window
+        nxtCDF = np.cumsum(winVals[idx+1, :])
+        
+        # Significance test on all windows
+        signCrit = np.max( ( np.abs(currCDF - prevCDF), 
+                                np.abs(currCDF - nxtCDF) ) )
     # Return KS test results
     return signCrit
