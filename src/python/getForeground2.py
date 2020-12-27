eb825055294c7a89c8d2a6c1f6e872606c40e4f4
jtzanetatos <58182829+jtzanetatos@users.noreply.github.com>
2020-12-27 02:23:23 +0200

    Update call script

--- src/python/getForeground2.py
+++ src/python/getForeground2.py
@@ -7,20 +7,13 @@ Created on Sun Feb 24 12:45:39 2019
 
 import numpy as np
 import cv2 as cv
-import my_functions as f
-# import os
+import objdect as obj
+import sys
 
 def main():
-    '''
     
-    
-    Returns
-    -------
-    None.
-    
-    '''
     # Initialize frame stream source
-    source = f.user_inpt()
+    source = obj.utils.ListSensors()
     cap = cv.VideoCapture(source)
     
     # 3 by 3 rectangular window for morph operators
@@ -28,8 +21,6 @@ def main():
     # Zivkovic MOG
     fgbg = cv.createBackgroundSubtractorMOG2(detectShadows = False)
     
-    # Better Results by increasing the influence of past frames (?)
-    #cv.BackgroundSubtractorMOG2.setHistory(fgbg, 1000)
     # contours px size to accept
     contour_size = 60
     
@@ -43,6 +34,7 @@ def main():
     # Initialize state of statistical tests
     mog2_stat = False
     dev_stat = False
+    
     # Initialize mask array
     prev_fgmask = np.full((480, 640), 255,dtype=np.uint8)
     # Initialize k-means frame
@@ -54,14 +46,6 @@ def main():
     curr_hist = np.zeros((256, 3), dtype=np.uint32)
     prev_hist = np.zeros((256, 3), dtype=np.uint32)
     
-    # Debugging counter for how many frames the MOG2 has converged
-    count = 0
-    # n_frame = 1
-    # dev_list = []
-    # mog2_list = []
-    # clst = []
-    # # Get current working directory
-    # main_path = os.getcwd()
     
     while True:
         # Capture frame-by-frame
@@ -75,54 +59,51 @@ def main():
             
             # Evaluate histograms of past & current frame
             for i in range(col):
-                curr_hist[::, i] = np.reshape(cv.calcHist(res2, [i], None, [256],
+                curr_hist[:, i] = np.reshape(cv.calcHist(res2, [i], None, [256],
                          [0, 256]), 256)
-                prev_hist[::, i] = np.reshape(cv.calcHist(prev_frame, [i], None,
+                prev_hist[:, i] = np.reshape(cv.calcHist(prev_frame, [i], None,
                      [256], [0, 256]), 256)
             
             # Filter current and previous frames histograms
-            filt_curr = f.hist_bpf(curr_hist)
-            filt_prev = f.hist_bpf(prev_hist)
+            filt_curr = obj.histogram_ops.HistBpf(curr_hist)
+            filt_prev = obj.histogram_ops.HistBpf(prev_hist)
             
             # Normalize current & previous frames filtered histograms
-            norm_curr = f.hist_norm(filt_curr)
-            norm_prev = f.hist_norm(filt_prev)
+            norm_curr = obj.histogram_ops.HistNorm(filt_curr)
+            norm_prev = obj.histogram_ops.HistNorm(filt_prev)
             
             # Evaluate if K-S test indicates the MOG2 has converged
-            mog2_stat = f.stop_mog2(norm_curr, norm_prev)
+            mog2_stat = obj.BG_converge.KSMoG2(norm_curr, norm_prev)
             
             # Implement plain & overlapping windows
-            (win_vals, win_binloc, over_vals, over_binloc) = f.windows(norm_curr)
+            win_vals, win_binloc = obj.histogram_ops.HistWindows(norm_curr)
             
-            # Rule based tree for windows of interest
-            (final_win_vals, final_win_loc) = f.rule_tree(win_vals, 
-                             win_binloc, over_vals, over_binloc)
-            
-            # Adaptive windows via means of K-S statistical test
-            _, out_win_vals, n_clusters = f.adapt_win(final_win_vals, final_win_loc, norm_curr,
-                win_vals, win_binloc, over_vals, over_binloc, deb_flg = 1)
+            # Adaptive windows
+            wind_vals, wind_bins = obj.CN2.CN2Entropy(win_vals, win_binloc, norm_curr)
             
             # Evaluate the result of the STD test
-            dev_stat = f.hist_deviation(out_win_vals)
+            # dev_stat = obj.BG_converge.HistDeviation(out_win_vals)
+            # Cluster estimator
+            # n_clusters = obj.CN2.KS_Tree.ClusterEstimator(wind_bins)
             
             # If MOG2 has converged
-            if mog2_stat and dev_stat and n_clusters > 2:
+            if mog2_stat and n_clusters > 2:
                 
                 # Implement K-means clustering to segment detected objects
-                # res_frame = f.kmeans_cv(frame, n_clusters)
-                # Increment counter by one
-                count += 1
+                res_frame = obj.image_processing.kmeans_sk(frame, n_clusters)
             else:
                 #apply MOG2 and get foreground pixels
                 fgmask = fgbg.apply(frame)
-                (res2, res, contours) = f.frame_proc(frame, fgmask, kernel, contour_size)
+                res2, res, contours = obj.image_processing.frame_proc(frame,
+                                                fgmask, kernel, contour_size)
                 
         else:
             # If MOG2 has not converge
-            if mog2_stat==False and dev_stat==False:
+            if mog2_stat==False:
                 #apply MOG2 and get foreground pixels
                 fgmask = fgbg.apply(frame)
-                (res2, res, contours) = f.frame_proc(frame, fgmask, kernel, contour_size)
+                res2, res, contours = obj.image_processing.frame_proc(frame,
+                                                fgmask, kernel, contour_size)
             
             # Apply previous mask on the current frame
     #        else:
@@ -136,7 +117,6 @@ def main():
         cv.imshow('Foreground', res2)
         cv.drawContours(res, contours, -1, (0, 0, 255), 2)
         cv.imshow('Contours', res)
-        cv.imshow('Raw Frame', frame)
         cv.imshow('K-Means results', res_frame)
         
     #    dev_list.append(dev_stat)
@@ -227,4 +207,4 @@ def main():
     #        file.write(str(s)+'\n')
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
